234df8fdcd8e0c8da1a8ea547e0ccfc004440ed0
Dave Bosley <dave.l.bosley@gmail.com>
2015-06-17 21:28:43 -0600

    It works!

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
 
     // Project configuration.
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+        pkg: grunt.file.readJSON('package.json')
         //uglify: {
         //    options: {
         //        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
