affcc1d54415ede93443d9728284d17796cdfa4a
Dave Bosley <dave.l.bosley@gmail.com>
2015-06-30 19:50:29 -0600

    Fixed Images

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,11 @@
 $(function () {
-
-
-
-
-
-    function DemoItem(url, name,picture) {
+    function DemoItem(url, name, picture) {
         var self = this;
 
         self.Url = ko.observable(url);
         self.Name = ko.observable(name);
         self.Picture = ko.observable(picture);
         self.Selected = ko.observable(false);
-
-
     }
 
     function ViewModel() {
@@ -20,18 +13,12 @@ $(function () {
         self.availableItems = ko.observableArray();
         self.associatedItem = ko.observable();
 
-
-
         self.associatedItem.subscribe(function (_associatedItem) {
             //put code here. each time a checkbox is marked, this is run.
             //With each run _associatedItemIds contains the ID of each DemoItem that has been checked
-
-            $('#zoom').attr("src","http://www.online-image-editor.com//styles/2014/images/example_image.png");
-
+            $('#zoom').attr("src", "images/kitten.png");
             self.getChartData(_associatedItem.Url(), self.renderChart);
-
         });
-
 
         self.getChartData = function (url, callback) {
             $.get(url, null, callback, "json");
@@ -41,57 +28,49 @@ $(function () {
             var ENERGYRingChart = dc.pieChart("#chart-ring-ENERGY"),
                 ENERGY2RingChart = dc.pieChart("#chart-ring-ENERGY2"),
                 CZRowChart = dc.rowChart("#chart-row-CZ");
-                totalchart = dc.numberDisplay("#totalchart");
+            totalchart = dc.numberDisplay("#totalchart");
             // use static or load via d3.csv
-
             // set crossfilter
             var ndx = crossfilter(chartData);
 
             LabelDim = ndx.dimension(function (d) {
-
                 return d.Label;
-            }),
-                HeatingDim = ndx.dimension(function (d) {
-                    return +d.Heat;
-                }),
+            });
 
-                WFDim = ndx.dimension(function (d) {
-                    return d.WF;
-                }),
-                EnergyPerYear = LabelDim.group().reduceSum(function (d) {
-                    return +d.Heat;
+            HeatingDim = ndx.dimension(function (d) {
+                return +d.Heat;
+            });
 
-                }),
-                HeatingPerYear = HeatingDim.group().reduceSum(function (d) {
-                    return +d.Heat;
+            WFDim = ndx.dimension(function (d) {
+                return d.WF;
+            });
 
-                }),
+            EnergyPerYear = LabelDim.group().reduceSum(function (d) {
+                return +d.Heat;
+            });
+            HeatingPerYear = HeatingDim.group().reduceSum(function (d) {
+                return +d.Heat;
+            });
 
-                PerZone = WFDim.group().reduceSum(function (d) {
-                    return +d.Heat;
-                }),
+            PerZone = WFDim.group().reduceSum(function (d) {
+                return +d.Heat;
+            });
 
             ENERGYRingChart
                 .width(600).height(600)
                 .dimension(LabelDim)
-                .title(function (d){
+                .title(function (d) {
                     return d.Label + " : " + d.value + "KW";
-                    }
-                )
+                })
                 .legend(dc.legend().x(250).y(200))
                 .group(EnergyPerYear)
-
-
                 .innerRadius(150);
-
-
 
             ENERGY2RingChart
                 .width(200).height(200)
                 .dimension(HeatingDim)
                 .group(HeatingPerYear)
                 .innerRadius(50);
-
 
             CZRowChart
                 .width(280).height(200)
@@ -109,23 +88,14 @@ $(function () {
             //});
 
             dc.renderAll();
-
-
-
-
-
-
-
-
-
         };
 
         self.init = function init() {
-            self.availableItems.push(new DemoItem("datafiles/1a.json", 'Miami','http://static.guim.co.uk/sys-images/Guardian/Pix/pictures/2014/4/11/1397210130748/Spring-Lamb.-Image-shot-2-011.jpg'));
-            self.availableItems.push(new DemoItem("datafiles/2a.json", 'Daluth',"http://www.online-image-editor.com//styles/2014/images/example_image.png"));
-            self.availableItems.push(new DemoItem("datafiles/5a.json", 'Chicago',"http://www.online-image-editor.com//styles/2014/images/example_image.png"));
-            self.availableItems.push(new DemoItem("datafiles/2b.json", 'Denver',"http://www.online-image-editor.com//styles/2014/images/example_image.png"));
-            self.availableItems.push(new DemoItem("datafiles/2b.json", 'SanteFe',"http://www.online-image-editor.com//styles/2014/images/example_image.png"));
+            self.availableItems.push(new DemoItem("datafiles/1a.json", 'Miami', 'images/lamb.jpg'));
+            self.availableItems.push(new DemoItem("datafiles/2a.json", 'Daluth', "images/kitten.png"));
+            self.availableItems.push(new DemoItem("datafiles/5a.json", 'Chicago', "images/kitten.png"));
+            self.availableItems.push(new DemoItem("datafiles/2b.json", 'Denver', "images/kitten.png"));
+            self.availableItems.push(new DemoItem("datafiles/2b.json", 'SanteFe', "images/kitten.png"));
 
             self.associatedItem(self.availableItems()[0]);
         };
@@ -133,7 +103,7 @@ $(function () {
 
     var viewModel = new ViewModel();
 
-    ko.applyBindings(viewModel);
     viewModel.init();
+    ko.applyBindings(viewModel);
     window.viewModel = viewModel;
 });
