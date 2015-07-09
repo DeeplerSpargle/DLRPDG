936eb4e115b2537f76a5e50a44069b7a64dffbb6
DeeplerSpargle <kobayshimaru@gmail.com>
2015-07-09 01:31:27 -0500

    END OF LINE

--- public/js/index.js
+++ public/js/index.js
@@ -48,7 +48,7 @@ $(function () {
             var ENERGYRingChart = dc.pieChart("#chart-ring-ENERGY"),
                 ENERGY2RingChart = dc.pieChart("#chart-ring-ENERGY2"),
                 WeatherFileRowChart = dc.rowChart("#chart-row-CZ");
-
+                dataTable = dc.dataTable("#dc-table-graph");
             // use static or load via d3.csv
             // set crossfilter
             var ndx = crossfilter(chartData);
@@ -77,12 +77,13 @@ $(function () {
             });
 
            ENERGYRingChart
-                .width(600).height(600)
+                .width(450).height(500)
                 .dimension(LabelDim)
+                .renderLabel(false)
                 .title(function (d) {
-                    return d.name + " : " + d.value + "KW";
+                    return d.key + " : " + d.value.toString();
                 })
-                .legend(dc.legend().x(250).y(200))
+                .legend(dc.legend().x(150).y(150))
                 .group(EnergyPerYear)
                 .innerRadius(150)
                 .on("filtered", function (chart) {
@@ -96,6 +97,7 @@ $(function () {
                 });
 
 
+
             ENERGY2RingChart
                 .width(200).height(200)
                 .dimension(HeatingDim)
@@ -103,8 +105,12 @@ $(function () {
                 .innerRadius(50);
 
             WeatherFileRowChart
-                .width(280).height(200)
+                .width(400).height(200)
                 .dimension(WFDim)
+                .title(function (d) {
+                    return d.key + " : " + d.value + "KW";
+                })
+                .renderLabel(true)
                 .group(PerZone)
                 .elasticX(true)
                 .renderlet(function (chart) {
@@ -114,10 +120,31 @@ $(function () {
                     dc.events.trigger(function () {
                         ENERGYRingChart.filter(chart.filter());
                     });
-                });
+                })
+                .xAxis().ticks(4);
+
+            dataTable
+                .width(800)
+                .height(800)
+                .dimension(HeatingDim)
+                .group(function(d){
+                   return "List of all Selected"
+                })
+                .size(100)
+                .columns([
+                    function(d) { return d.WF; },
+                    function(d) { return d.Label; },
+                    function(d) { return d.Heat; }
+
+                ])
+                .sortBy(function(d){ return d.WF; })
+                // (optional) sort order, :default ascending
+                .order(d3.ascending);
+
 
 
             dc.renderAll();
+
         };
 
         self.init = function init() {
@@ -130,12 +157,17 @@ $(function () {
             self.availableItems.push(new DemoItem("datafiles/5a.json", 'Baltimore',   "images/4A BALTIMORE/Climate Files7.jpg"));
             self.availableItems.push(new DemoItem("datafiles/2b.json", 'Albuquerque',    "images/4B ALBUQUERQUE/Climate Files8.jpg"));
             self.availableItems.push(new DemoItem("datafiles/2b.json", 'Portland',   "images/4C PORTLAND/Climate Files9.jpg"));
-
-
+            self.availableItems.push(new DemoItem("datafiles/2a.json", 'Chicago',    "images/5A CHICAGO/Climate Files10.jpg"));
+            self.availableItems.push(new DemoItem("datafiles/5a.json", 'Boulder',   "images/5B BOULDER/Climate Files11.jpg"));
+            self.availableItems.push(new DemoItem("datafiles/2b.json", 'Vancouver',    "images/5C VANCOUVER/Climate Files12.jpg"));
+            self.availableItems.push(new DemoItem("datafiles/2b.json", 'Minneapolis',   "images/6A MINNEAPOLIS/Climate Files13.jpg"));
+            self.availableItems.push(new DemoItem("datafiles/5a.json", 'Helena',   "images/6B HELENA/Climate Files14.jpg"));
+            self.availableItems.push(new DemoItem("datafiles/2b.json", 'Duluth',    "images/7 DULUTH/Climate Files15.jpg"));
+            self.availableItems.push(new DemoItem("datafiles/2b.json", 'Fairbanks',   "images/8 FAIRBANKS/Climate Files16.jpg"));
 
             self.availableBuildings.push(new BuildingItem("BUILDING TYPE I","images/1A MIAMI/1A_MAX_WR.png"));
-            self.availableBuildings.push(new BuildingItem("BUILDING TYPE II","images/kitten.png"));
-            self.availableBuildings.push(new BuildingItem("BUILDING TYPE III","images/lamb.jpg"));
+            self.availableBuildings.push(new BuildingItem("BUILDING TYPE II","images/2A HOUSTON/2A_MAX_WR.png"));
+            self.availableBuildings.push(new BuildingItem("BUILDING TYPE III","images/2B PHOENIX/2B_MAX_WR.png"));
 
 
 
